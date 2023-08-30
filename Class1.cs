e7913b9635f59c45a2411cb6b54b70a2ce187292
yf836760 <yf836760@outlook.com>
2023-08-30 13:35:56 +0800

    Initial TShock project

--- Class1.cs
+++ Class1.cs
@@ -1,5 +1,63 @@
-﻿namespace CustomItem;
-public class Class1
-{
+﻿﻿using TerrariaApi.Server;
+using Terraria;
 
+namespace CustomItem;
+/// <summary>
+/// The main plugin class should always be decorated with an ApiVersion attribute. The current API Version is 2.1
+/// </summary>
+[ApiVersion(2, 1)]
+public class CustomItem : TerrariaPlugin
+{
+    /// <summary>
+    /// The name of the plugin.
+    /// </summary>
+    public override string Name => "CustomItem Plugin";
+
+    /// <summary>
+    /// The version of the plugin in its current state.
+    /// </summary>
+    public override Version Version => new Version(1, 0, 0);
+
+    /// <summary>
+    /// The author(s) of the plugin.
+    /// </summary>
+    public override string Author => "yf836760";
+
+    /// <summary>
+    /// A short, one-line, description of the plugin's purpose.
+    /// </summary>
+    public override string Description => "Custom item like weapons in terraria";
+
+    /// <summary>
+    /// The plugin's constructor
+    /// Set your plugin's order (optional) and any other constructor logic here
+    /// </summary>
+    public CustomItem(Main game) : base(game)
+    {
+
+    }
+
+    /// <summary>
+    /// Performs plugin initialization logic.
+    /// Add your hooks, config file read/writes, etc here
+    /// </summary>
+    public override void Initialize()
+    {
+        throw new NotImplementedException();
+    }
+        
+    /// <summary>
+    /// Performs plugin cleanup logic
+    /// Remove your hooks and perform general cleanup here
+    /// </summary>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            //unhook
+            //dispose child objects
+            //set large objects to null
+        }
+        base.Dispose(disposing);
+    }
 }
